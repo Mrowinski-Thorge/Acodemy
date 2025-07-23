99e8d9053317363a35a3d9f896f3215b768af04b
Mrowinski-Thorge <Mrowinski-Thorge@users.noreply.github.com>
2025-07-23 20:53:32 +0000

    deploy: 02556c93af1e0758cdd3b9fb7812c70d7deab867

---
+++ config.js
@@ -0,0 +1,10 @@
+// !!! Automatisch erzeugt !!!
+window.firebaseConfig = {
+  apiKey: 'AIzaSyDrbeHimI43xjAaw-PvxtZUDh0v3bZA4XQ',
+  authDomain: 'codegame-dev.firebaseapp.com',
+  projectId: 'codegame-dev',
+  storageBucket: 'codegame-dev.firebasestorage.app',
+  messagingSenderId: '787254922496',
+  appId: '1:787254922496:web:e4728c517363197eb7f7e5',
+  measurementId: 'G-R3X0TKGNZV'
+};
